5d8ebae38495d2a5219ebb182891e2e628fd4acb
luigi sato <base.basketball0607@gmail.com>
2022-04-06 21:27:27 +0900

    Initial Commit

---
+++ SubscriptionManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubscriptionManagement
+//
+//  Created by 佐藤瑠偉史 on 2022/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
